f8f5b5586048a0287b03ad7c7fa5b4dfc2c0045b
Christian Köberl <christian.koeberl@porscheinformatik.com>
2023-06-19 12:39:00 +0200

    Spring Boot 3.1, write all headers

--- src/main/java/com/porscheinformatik/koc/snoop/snoop/SnoopApplication.java
+++ src/main/java/com/porscheinformatik/koc/snoop/snoop/SnoopApplication.java
@@ -1,14 +1,16 @@
 package com.porscheinformatik.koc.snoop.snoop;
 
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
+import jakarta.servlet.http.HttpServletRequest;
 
 @SpringBootApplication
 @RestController
@@ -23,7 +25,7 @@ public class SnoopApplication {
         Map<String, Object> headers = new HashMap<>();
         for (Enumeration<String> headerNames = request.getHeaderNames(); headerNames.hasMoreElements(); ) {
             String headerName = headerNames.nextElement();
-            headers.put(headerName, request.getHeader(headerName));
+            headers.put(headerName, Collections.list(request.getHeaders(headerName)));
         }
 
         return Map.of("headers", headers, "remoteAddr", request.getRemoteAddr(), "remoteHost", request.getRemoteHost(), "remotePort", request.getRemotePort());
